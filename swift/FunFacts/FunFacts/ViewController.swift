de0c8af798b9b603c3fb1b69ef0a6d9694dd0f2c
kinnik <northislandking@gmail.com>
2014-09-27 21:05:52 +0100

    Completion of Getting Started with Xcode

---
+++ swift/FunFacts/FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Harry James Potter on 27/09/2014.
+//  Copyright (c) 2014 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
